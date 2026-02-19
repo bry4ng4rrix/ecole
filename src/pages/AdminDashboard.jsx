ae9a5c510c48bf14e9df6443058abe7a7f704b23
bry4ng4rrix <bryanmfb4@gmail.com>
2026-02-19 11:06:25 +0300

    s

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -5,10 +5,9 @@ import {
   Calendar, CheckCircle, AlertCircle, Home, Layers, Gauge, Database, Eye, Bell
 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent,  CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Badge } from '@/components/ui/badge'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import {
